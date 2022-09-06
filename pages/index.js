8ef8daf9a9a0e7955f9fddd819f291bee83067e8
Lucas Eduardo <lucaseduardo@MacBook-Air-de-Lucas.local>
2022-09-06 10:16:43 -0300

    Adicionando nome do pokemon em japones

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,23 @@ import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Color from 'color-thief-react'
 import Loader from '../src/components/loader';
+import pokemonName from "pokemon"
 
 export default function Home() {
   const [searchPokemon, setSearchPokemon] = useState("");
+  const [chinesePokemon, setChinesePokemon] = useState("");
   const [pokemon, setPokemon] = useState([]);
   const [loading, setLoading] = useState(true);
   const IMAGE_URI = pokemon.sprites?.other["official-artwork"].front_default
 
+
   const getPokemon = (name) => {
     setLoading(true)
     axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
-      .then(({data}) => {
+      .then(({ data }) => {
         setPokemon(data)
         setLoading(false)
+        setChinesePokemon(pokemonName.getName(pokemon.id || 143, "ja"))
       })
       .catch(() => {
         alert("Pokemon não encontrado.")
@@ -28,26 +32,30 @@ export default function Home() {
   }
 
   useEffect(() => {
-    getPokemon("mewtwo")
+    getPokemon("snorlax")
   }, [])
 
   if (loading) {
-    return <Loader/>
+    return <Loader />
   }
 
   return (
     <Color src={IMAGE_URI} crossOrigin="anonymous" format="hex">
       {({ data, loading }) => {
-        if (loading) return <Loader/>
+        if (loading) return <Loader />
 
         return (
           <div className={styles.container} style={{ backgroundColor: data }}>
             <div className={styles.header}>
-              <span className={styles.title} style={{ padding: "5rem" }}>{pokemon.name}</span>
+              <span className={styles.identify} style={{ paddingLeft: "5rem" }}>#{pokemon.id}</span>
+              <span className={styles.title} style={{ paddingLeft: "5rem" }}>{pokemon.name}</span>
             </div>
             <div className={styles.main}>
               <div className={styles.leftSide}>
                 <div className={styles.infoLeftSide}>
+                  <span className={styles.chineseText}>
+                    {chinesePokemon}
+                  </span>
                   <span className={styles.fontInfo}>Height: {pokemon.height}m</span>
                   <span className={styles.fontInfo}>Weight: {pokemon.weight}kg</span>
                 </div>
@@ -70,8 +78,8 @@ export default function Home() {
             </div>
 
             <form onSubmit={(e) => e.preventDefault()} style={{ display: "flex", alignItems: "center", gap: 10 }}>
-              <input required value={searchPokemon} onChange={(e) => setSearchPokemon(e.target.value)} placeholder="Gengar"/>
-              <input type={"submit"} onClick={() => {if (searchPokemon) handleSearchPokemon()}} value={"Pesquisar"} />
+              <input className={styles.inputStyle} required value={searchPokemon} onChange={(e) => setSearchPokemon(e.target.value)} placeholder="Gengar" />
+              <input className={styles.inputStyle} style={{ cursor: 'pointer' }} type={"submit"} onClick={() => { if (searchPokemon) handleSearchPokemon() }} value={"GO!"} />
             </form>
           </div>
         )
